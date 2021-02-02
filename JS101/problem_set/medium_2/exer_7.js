f40a969042b9e7c9fd248745da94fa9902c36b72
Jordan Binder <binder.jordan@gmail.com>
2021-02-02 10:45:12 -0800

    practice problems

--- JS101/problem_set/medium_2/exer_7.js
+++ JS101/problem_set/medium_2/exer_7.js
@@ -22,9 +22,36 @@ output is array sorted using bubble sort
 D - 
 we will want to pair two vars in an array in our loop
 
+high level:
+
+for loop - idx 0, idx <= length of array; increment by 1
+
+Questions: What condition will make it stop?
+result === array.sort((a, b) => a - b) ???
 */
 
-function bubbleSort(array)
+function bubbleSort(array) {
+  
+  for (let idx = 0; idx <= array.length; idx++) {
+    let pair = [array[idx], array[idx + 1]];
+    if (array[idx] > array[idx + 1]) {
+      array = [array[idx + 1], array[idx]];
+    }
+  }
+}
+
+function bubbleSort(array) {
+  while (true) {
+    let swapped = false;
+    for (let idx = 1; idx < array.length; idx++) {
+      if (array[idx - 1] <= array[idx]) continue;
+      [array[idx - 1], array[idx]] = [array[idx], array[idx - 1]];
+      swapped = true;
+    }
+    if (!swapped) break;
+  }
+}
+
 
 let array1 = [5, 3];
 bubbleSort(array1);
@@ -37,3 +64,12 @@ console.log(array2);    // [1, 2, 4, 6, 7]
 let array3 = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie'];
 bubbleSort(array3);
 console.log(array3);    // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
+
+/* post mortem:
+
+My two big issues were not thinking to use a while loop for the outer loop.
+I was thinking I could find a condition that would stop the array. 
+Comparing the final array to the sorted would work, but that would seem like defeating the purpose
+of the bubble sort as an exercise.
+
+*/
---
+++ JS101/problem_set/medium_2/exer_8.js
@@ -0,0 +1,55 @@
+/*
+P -Understand the Problem
+E -Examples / Test cases
+D -Data Structure
+A -Algorithm
+C -Code
+*/
+
+/*
+P -
+Input:
+
+Output:
+
+Explicit requirements:
+
+Implicit Requirements:
+
+Questions:
+What kind of loop/iteration might be good for this question (if any)?
+
+
+Can this be made easier/more readable by breaking it up into more functions?
+
+
+E - do a test case to see if everything is working
+
+D - What kind of data structures will you need to create or manipulate to create
+working function?
+
+
+A - algorithm
+
+First, high level:
+
+
+Second, pseudocode:
+
+
+C - code
+
+
+Question:
+
+Notes:
+
+*/
+
+function StrawberrySalad() {
+
+}
+
+/* Postmortem:
+
+*/
