0bb7257a0dd389b5278f54124de53b795a952065
Jordan Binder <binder.jordan@gmail.com>
2021-03-04 20:13:38 -0800

    start of advanced exercises

---
+++ JS101/problem_set/advanced_1/exer_1.js
@@ -0,0 +1,42 @@
+/*
+P:
+E:
+D:
+template of string with object literal
+adj arr
+noun arr
+verbs arr
+adverbs arr
+
+A:
+create var of string with object literal. This is template
+
+C:
+*/
+
+function madlibs(template) {
+  // ...
+}
+
+// These examples use the following list of replacement texts:
+adjectives: quick lazy sleepy noisy hungry
+nouns: fox dog head leg tail cat
+verbs: jumps lifts bites licks pats
+adverbs: easily lazily noisily excitedly
+------
+
+madlibs(template1);
+// The ${adjectives[Math.abs(Math.random - 1) * adjectives.length]} brown ${nouns} ${adverbs}
+// "licks" the "sleepy" yellow
+// "dog", who "lazily" "licks" his
+// "tail" and looks around.
+
+madlibs(template1);
+// The "hungry" brown "cat" "lazily"
+// "licks" the "noisy" yellow
+// "dog", who "lazily" "licks" his
+// "leg" and looks around.
+
+madlibs(template2);      // The "fox" "bites" the "dog"'s "tail".
+
+madlibs(template2);      // The "cat" "pats" the "cat"'s "head".
